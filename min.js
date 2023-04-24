9a80e7695cd59bc2645bf00725854b67ec65db7e
KrancoConF <franco.rodriguez.gallipoli@gmail.com>
2023-04-24 18:06:22 -0300

    Prototipo funcional con HTML, JS y Bootstrap

--- min.js
+++ min.js
@@ -1,44 +1,48 @@
-const div = document.getElementById('container');
-const bt1 = document.getElementById('bt1');
-const bt2 = document.getElementById('bt2');
-const bt3 = document.getElementById('bt3');
-
 var STPA = {
     pagina:[
         {
-            value:1,
+            value:0,
             text:"Estás en un pasillo oscuro con una brújula en mano. Al norte hay una luz distante, y hacia el sur una oscuridad infinita.",
-            boton1:{dest:2,text:"Avanzar"},
-            boton2:{dest:3,text:"Retroceder"},
-            boton3:{dest:4,text:"Dar Vueltas"}
+            b1:{dest:1,text:"Avanzar"},
+            b2:{dest:2,text:"Retroceder"}
+        },
+        {
+            value:1,
+            text:"Das con una puerta cerrada. Necesitás una llave.",
+            b1:{dest:0,text:"Volver al inicio"},
+            b2:{dest:4,text:"Dar Vueltas"}
         },
         {
             value:2,
-            text:"Das con una puerta cerrada. Necesitás una llave.",
-            boton1:{dest:1,text:"Volver al inicio"},
-            boton2:{dest:4,text:"Dar Vueltas"}
+            text:"La oscuridad te traga y el entorno no cambia sin importar cuanto camines.",
+            b1:{dest:0,text:"Volver al inicio"},
+            b2:{dest:3,text:"Dar Vueltas"}
         },
         {
             value:3,
-            text:"La oscuridad te traga y el entorno no cambia sin importar cuanto camines.",
-            boton1:{dest:1,text:"Volver al inicio"},
-            boton2:{dest:4,text:"Dar Vueltas"}
-        },
-        {
-            value:4,
             text:"Das vueltas.",
-            boton1:{dest:1,text:"Volver al inicio"},
-            boton2:{dest:4,text:"Dar Vueltas"}
+            b1:{dest:0,text:"Volver al inicio"},
+            b2:{dest:3,text:"Dar Vueltas"}
         }
     ]
 };
 
+var image = document.querySelector('#image');
+var text = document.querySelector('#text');
+var bt1 = document.querySelector('#bt1');
+var bt2 = document.querySelector('#bt2');
+
 function changeText(){
     let btVal = this.value;
-    console.log("Valor: "+btVal);
-    div.innerHTML = STPA.pagina[btVal].boton1.text;
+    let stpa = STPA.pagina[btVal];
+    console.log(stpa);
+    console.log(stpa.text);
+    text.innerHTML = stpa.text;
+    bt1.innerHTML = stpa.b1.text;
+    bt2.innerHTML = stpa.b2.text;
+    bt1.setAttribute('value',stpa.b1.dest);
+    bt2.setAttribute('value',stpa.b2.dest);
 }
 
 bt1.addEventListener('click', changeText);
 bt2.addEventListener('click', changeText);
-bt3.addEventListener('click', changeText);
